62e3d900e1ee1b3ab56b5b201e47bbac678c1610
estebanr14 <estebanrestrepor.14@gmail.com>
2019-12-14 14:32:58 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,30 @@
+
+const express = require('express');//Importa el módulo express
+const bodyParser = require('body-parser'); //Importa el módulo body-parse
+const cors = require('cors');
+
+
+
+//Inicializa express
+const app = express();
+
+app.use(cors());
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+// parse application/json
+app.use(bodyParser.json());
+
+app.post('/test', (req,res)=>{
+    res.json(req.body);
+});
+
+app.get('/test', (req,res)=>{
+    res.send('Hello bitch!');
+});
+
+//Crea el servidor por el puerto port
+const port =  process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log('*** Server is up and runnign on port number ' + port + '\n');
+});
