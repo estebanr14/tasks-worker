1e4483c8d74852648fb5dc33a4cde320cfadd4cf
estebanr14 <estebanrestrepor.14@gmail.com>
2019-12-16 15:32:05 -0500

    parsers

--- app.js
+++ app.js
@@ -10,10 +10,9 @@ const app = express();
 
 app.use(cors());
 
-// parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: false }));
-// parse application/json
 app.use(bodyParser.json());
+app.use(bodyParser.raw());
+app.use(bodyParser.text());
 
 app.post('/test', (req,res)=>{
     console.log('////////////////////////////////////////////////////')
